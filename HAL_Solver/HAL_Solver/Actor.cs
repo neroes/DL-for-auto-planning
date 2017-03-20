257573cea3d4ce49e6883d753afc871fe4f3924a
neroes <sorenjesperbloch@msn.com>
2017-03-20 14:29:54 +0100

    added remaining direction

--- HAL_Solver/HAL_Solver/Actor.cs
+++ HAL_Solver/HAL_Solver/Actor.cs
@@ -56,7 +56,12 @@ namespace HAL_Solver
                 if (isempty[1,2]) { actionList.Add(new act(Interact.MOVE, Direction.S)); }
                 else if (currentMap.isBox(x, y - 1, getcolor(), out box))
                 {
-                   
+                    if (isempty[2, 2]) { actionList.Add(new act(Interact.PUSH, Direction.E, Direction.S, box)); }
+                    if (isempty[0, 2]) { actionList.Add(new act(Interact.PUSH, Direction.W, Direction.S, box)); }
+                    if (currentMap.isEmptySpace(x, y - 2)) { actionList.Add(new act(Interact.PUSH, Direction.S, Direction.S, box)); }
+                    if (isempty[1, 0]) { actionList.Add(new act(Interact.PULL, Direction.N, Direction.S, box)); }
+                    if (isempty[2, 1]) { actionList.Add(new act(Interact.PULL, Direction.E, Direction.S, box)); }
+                    if (isempty[0, 1]) { actionList.Add(new act(Interact.PULL, Direction.W, Direction.S, box)); }
                 }
             }
             if (!currentMap.isWall(x + 1, y))
@@ -64,7 +69,12 @@ namespace HAL_Solver
                 if (isempty[2,1]) { actionList.Add(new act(Interact.MOVE, Direction.E)); }
                 else if (currentMap.isBox(x + 1, y, getcolor(), out box))
                 {
-                    
+                    if (isempty[2, 0]) { actionList.Add(new act(Interact.PUSH, Direction.N, Direction.E, box)); }
+                    if (isempty[2, 2]) { actionList.Add(new act(Interact.PUSH, Direction.S, Direction.E, box)); }
+                    if (currentMap.isEmptySpace(x + 2, y)) { actionList.Add(new act(Interact.PUSH, Direction.E, Direction.E, box)); }
+                    if (isempty[0, 1]) { actionList.Add(new act(Interact.PULL, Direction.W, Direction.E, box)); }
+                    if (isempty[1, 0]) { actionList.Add(new act(Interact.PULL, Direction.N, Direction.E, box)); }
+                    if (isempty[1, 2]) { actionList.Add(new act(Interact.PULL, Direction.S, Direction.E, box)); }
                 }
             }
             if (!currentMap.isWall(x - 1, y))
@@ -72,9 +82,15 @@ namespace HAL_Solver
                 if (isempty[0, 1]) { actionList.Add(new act(Interact.MOVE, Direction.W)); }
                 else if (currentMap.isBox(x - 1, y, getcolor(), out box))
                 {
-                    
+                    if (isempty[0, 0]) { actionList.Add(new act(Interact.PUSH, Direction.N, Direction.W, box)); }
+                    if (isempty[0, 2]) { actionList.Add(new act(Interact.PUSH, Direction.S, Direction.W, box)); }
+                    if (currentMap.isEmptySpace(x - 1, y)) { actionList.Add(new act(Interact.PUSH, Direction.W, Direction.W, box)); }
+                    if (isempty[2, 1]) { actionList.Add(new act(Interact.PULL, Direction.E, Direction.W, box)); }
+                    if (isempty[1, 0]) { actionList.Add(new act(Interact.PULL, Direction.N, Direction.W, box)); }
+                    if (isempty[1, 2]) { actionList.Add(new act(Interact.PULL, Direction.S, Direction.W, box)); }
                 }
             }
+            actionList.Add(new act(Interact.WAIT));
             return actionList;
         }
     }
@@ -95,6 +111,13 @@ namespace HAL_Solver
         {
             this.inter = inter;
             this.dir = dir;
+            this.boxdir = Direction.NONE;
+            this.box = 255;
+        }
+        public act(Interact inter)
+        {
+            this.inter = inter;
+            this.dir = Direction.NONE;
             this.boxdir = Direction.NONE;
             this.box = 255;
         }
