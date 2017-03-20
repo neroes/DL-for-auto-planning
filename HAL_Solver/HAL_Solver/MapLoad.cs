1f19562dea8dc3862247f13ec552c497d2f94296
Emil-M <s154202@student.dtu.dk>
2017-03-20 12:36:15 +0100

    MapLoad stuff

--- HAL_Solver/HAL_Solver/MapLoad.cs
+++ HAL_Solver/HAL_Solver/MapLoad.cs
@@ -19,7 +19,6 @@ namespace HAL_Solver
             Collection<Actor> newactors = new Collection<Actor>();
             GoalList newgoals = new GoalList();
             Dictionary<char, Color> colorDict = new Dictionary<char, Color>();
-            byte actorid = 0;
             bool[] newwallmap = new bool[colcount*rowcount];
 
             Collection<char> boxnames = new Collection<char>();
@@ -30,16 +29,14 @@ namespace HAL_Solver
                 
                 if (line.Contains("+"))
                 {
-                    
-
                     Byte i = 0; // col count
                     //map construction
                     foreach (char c in line)
                     {
                         if (c == '+') { newwallmap[i+ j*colcount] = true; }
-                        else if (Char.IsLower(c)) { newgoals.Add(i, j, c); } // i,j is goal
+                        else if (Char.IsLower(c)) { newgoals.Add(i, j, Char.ToUpper(c)); } // i,j is goal
                         else if (Char.IsDigit(c)) {// i,j is actor
-                            if (colorDict.ContainsKey(c)) { newactors.Add(new Actor(i,j,actorid++)); }
+                            if (colorDict.ContainsKey(c)) { newactors.Add(new Actor(i,j,(byte)(c - '0'))); }
                         } 
                         else if (Char.IsUpper(c)) {
                             if (colorDict.ContainsKey(c)) { newboxes.Add(new Node(i,j)); }
@@ -59,13 +56,12 @@ namespace HAL_Solver
                     {
                         colorDict[name[0]] = Color.FromName(splitline[0]);
                     }
-                    //do color devision
                 }
             }
             map = new Map(newwallmap, colcount, newactors, newboxes, boxnames, newgoals, colorDict);
 
         }
-        public static void getfilesize(string filename,out int colcount,out int rowcount)
+        public static void getfilesize(string filename, out int colcount, out int rowcount)
         {
             colcount = 0;
             rowcount = 0;
@@ -74,7 +70,7 @@ namespace HAL_Solver
                 
                 if (line.Contains("+")) {
                     rowcount++;
-                    if (colcount< line.Length) { colcount = line.Length; }
+                    if (colcount < line.Length) { colcount = line.Length; }
                 }
             }
         }
