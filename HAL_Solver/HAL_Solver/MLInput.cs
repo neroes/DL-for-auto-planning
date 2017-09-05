28692fc621eb464ef85d84394eb8116e0d8f3d0a
Soren Jesper Bloch <sorenjesperbloch@msn.com>
2017-09-05 13:03:25 +0200

    added to ML input code

--- HAL_Solver/HAL_Solver/MLInput.cs
+++ HAL_Solver/HAL_Solver/MLInput.cs
@@ -12,10 +12,27 @@ namespace HAL_Solver
         public MLInput(Map map)
         {
             container = new cellpoint[70 * 70];
-            foreach (Node box in map.getAllBoxes())
+            Node[] boxes = map.getAllBoxes();
+            int j = 0;
+            while (j < Map.wallMap.Length)
+            {
+                for (int i = 0; i < Map.mapWidth; i++)
+                {
+                    container[i + j / Map.mapWidth * 70][1] = Map.wallMap[i + j / Map.mapWidth];
+                    container[i + j / Map.mapWidth * 70][0] = container[i + j / Map.mapWidth * 70][1] != true;
+                    j++;
+                }
+            }
+            foreach (Actor actor in map.getActors())
+            {
+                container[actor.x + actor.y * 70][2 + actor.id]=true;
+                container[actor.x + actor.y * 70][60 + (byte)actor.getcolor()]=true;
+            }
+            for (int i = 0; i<boxes.Length; i++)
             {
                 
             }
+            
             foreach (char name in map.getGoalNames())
             {
                 foreach (Node goal in map.getGoals(name))
@@ -23,10 +40,7 @@ namespace HAL_Solver
 
                 }
             }
-            foreach (Actor box in map.getActors())
-            {
-
-            }
+            
             
         }
     }
