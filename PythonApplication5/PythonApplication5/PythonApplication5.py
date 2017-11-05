f6377a7f078d745f12448d3c10fc9afc394d88db
neroes <sorenjesperbloch@msn.com>
2017-11-05 13:26:00 +0100

    forgot something

--- PythonApplication5/PythonApplication5/PythonApplication5.py
+++ PythonApplication5/PythonApplication5/PythonApplication5.py
@@ -102,6 +102,8 @@ def cnn_model_fn(features, labels, mode):
   onehot_labels = tf.one_hot(indices=tf.cast(labels, tf.int32), depth=102)
   loss = tf.losses.softmax_cross_entropy(
       onehot_labels=onehot_labels, logits=logits)
+  global_step = tf.Variable(0, trainable=False)
+  starter_learning_rate = 0.1
   learning_rate = tf.train.exponential_decay(starter_learning_rate, global_step,
                                            100000, 0.96, staircase=True)
   # Configure the Training Op (for TRAIN mode)
