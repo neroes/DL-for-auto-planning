7d05ae3d92835b1a57b08f2d80761bdf71012492
Joncapion <jon@hedenquist.dk>
2017-11-18 22:54:00 +0100

    trying with another conv/pool layer

--- testscript/PythonApplication5/testscript.py
+++ testscript/PythonApplication5/testscript.py
@@ -38,8 +38,8 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 28, 28, 32]
   conv1 = tf.layers.conv2d(
       inputs=input_layer,
-      filters=64,
-      kernel_size=[3, 3],
+      filters=32,
+      kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
 
@@ -56,8 +56,8 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 14, 14, 64]
   conv2 = tf.layers.conv2d(
       inputs=pool1,
-      filters=128,
-      kernel_size=[3, 3],
+      filters=64,
+      kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
 
@@ -74,25 +74,35 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 14, 14, 64]
   conv3 = tf.layers.conv2d(
       inputs=pool2,
-      filters=256,
-      kernel_size=[3, 3],
+      filters=128,
+      kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
 
   # Pooling Layer #3
   pool3 = tf.layers.max_pooling2d(inputs=conv3, pool_size=[2, 2], strides=2)
 
+  conv4 = tf.layers.conv2d(
+      inputs=pool3,
+      filters=256,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+
+  # Pooling Layer #3
+  pool4 = tf.layers.max_pooling2d(inputs=conv4, pool_size=[2, 2], strides=2)
+
   # Flatten tensor into a batch of vectors
   # Input Tensor Shape: [batch_size, 7, 7, 64]
   # Output Tensor Shape: [batch_size, 7 * 7 * 64]
-  pool3_flat = tf.reshape(pool3, [-1, 2*2*256])
+  pool4_flat = tf.reshape(pool4, [-1, 1*1*256])
 
 
   # Dense Layer
   # Densely connected layer with 1024 neurons
   # Input Tensor Shape: [batch_size, 7 * 7 * 64]
   # Output Tensor Shape: [batch_size, 1024]
-  dense = tf.layers.dense(inputs=pool3_flat, units=1024, activation=tf.nn.relu)
+  dense = tf.layers.dense(inputs=pool4_flat, units=1024, activation=tf.nn.relu)
 
   # Add dropout operation; 0.6 probability that element will be kept
   dropout = tf.layers.dropout(
