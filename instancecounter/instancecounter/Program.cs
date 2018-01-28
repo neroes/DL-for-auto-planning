46a1bb4cb49f3c1c87ef4e9b9964f3ae8445adb3
Soren Jesper Bloch <sorenjesperbloch@msn.com>
2018-01-28 21:43:22 +0100

    added instance counter

---
+++ instancecounter/instancecounter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace instancecounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> count = new Dictionary<string, int>();
+            List<String> names = new List<string>();
+            StreamReader sr = new StreamReader(args[0]);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] splitline = line.Split(' ');
+                if (count.ContainsKey(splitline[1]))
+                {
+                    count[splitline[1]]++;
+                }
+                else
+                {
+                    count[splitline[1]] = 1;
+                    names.Add(splitline[1]);
+                }
+            }
+            StreamWriter sw = new StreamWriter("output.txt");
+            foreach (String name in names)
+            {
+                sw.WriteLine(name + " " + count[name]);
+            }
+            sw.Flush();
+        }
+    }
+}
