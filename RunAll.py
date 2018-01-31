8f8207bf0a4ddd2839b109dccdb536a0360ce73b
Soren Jesper Bloch <sorenjesperbloch@msn.com>
2018-02-01 00:53:19 +0100

    just run classifiers

--- RunAll.py
+++ RunAll.py
@@ -12,17 +12,17 @@ os.chdir(thisfolder)
 
 os.chdir("./Classifier/testscript3D/PythonApplication5/")
 os.system("python Repeater.py 20 20000")
-os.chdir(thisfolder)
+#os.chdir(thisfolder)
 
-os.chdir("./Regression/testscript2D/PythonApplication5/")
-os.system("python Repeater.py 20 20000")
-os.chdir(thisfolder)
+#os.chdir("./Regression/testscript2D/PythonApplication5/")
+#os.system("python Repeater.py 20 20000")
+#os.chdir(thisfolder)
 
-os.chdir("./Regression/testscript2.5D/PythonApplication5/")
-os.system("python Repeater.py 20 20000")
-os.chdir(thisfolder)
+#os.chdir("./Regression/testscript2.5D/PythonApplication5/")
+#os.system("python Repeater.py 20 20000")
+#os.chdir(thisfolder)
 
-os.chdir("./Regression/testscript3D/PythonApplication5/")
-os.system("python Repeater.py 20 20000")
+#os.chdir("./Regression/testscript3D/PythonApplication5/")
+#os.system("python Repeater.py 20 20000")
 
 raw_input('Press Enter to exit')
