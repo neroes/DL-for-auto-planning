63d5e708084ea86a5b95976bf027a55d9eb487dc
Soren Jesper Bloch <sorenjesperbloch@msn.com>
2018-02-05 14:57:42 +0100

    added network save stepsize

--- Classifier/testscript2D/PythonApplication5/Repeater.py
+++ Classifier/testscript2D/PythonApplication5/Repeater.py
@@ -2,15 +2,17 @@ import sys
 import os
 import shutil
 import testscript
-
+# this function runs the network x times with y steps and saves it every z runs
 if __name__ == "__main__":
     x = int(sys.argv[1])
     y = int(sys.argv[2])
+    z = int(sys.argv[3])
     # call = "python testscript.py " + str(y)
     storage_location = os.environ['HOME']+"/storage/Classifier/2D/"
     testscript.numOfSteps=y
     for i in range(0,x):
         # os.system(call)
         testscript.main()
-        shutil.copytree("/tmp/Classifier/2D", storage_location+str(y*(i+1)), ignore=None)
+        if (i%z == z-1 or i == x):
+            shutil.copytree("/tmp/Classifier/2D", storage_location+str(y*(i+1)), ignore=None)
         shutil.copy("./results.txt",storage_location+str(y*(i+1)))
