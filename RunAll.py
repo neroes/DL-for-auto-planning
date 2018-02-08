4713b0297bcb1c8b575c8057acf0e7fa02dfbefe
Soren Jesper Bloch <sorenjesperbloch@msn.com>
2018-02-08 16:00:14 +0100

    made it save every 20 stop instead of every 5

--- RunAll.py
+++ RunAll.py
@@ -3,15 +3,15 @@ import os
 thisfolder =os.getcwd()
 
 os.chdir("./Classifier/testscript2D/PythonApplication5/")
-os.system("python Repeater.py 320 5000 5")
+os.system("python Repeater.py 320 5000 20")
 os.chdir(thisfolder)
 
 os.chdir("./Classifier/testscript2.5D/PythonApplication5/")
-os.system("python Repeater.py 320 5000 5")
+os.system("python Repeater.py 320 5000 20")
 os.chdir(thisfolder)
 
 os.chdir("./Classifier/testscript3D/PythonApplication5/")
-os.system("python Repeater.py 320 5000 5")
+os.system("python Repeater.py 320 5000 20")
 #os.chdir(thisfolder)
 
 #os.chdir("./Regression/testscript2D/PythonApplication5/")
