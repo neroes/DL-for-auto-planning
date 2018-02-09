ed7b921f5a2da961fd5ed6db6747560c93f78177
Soren Jesper Bloch <sorenjesperbloch@msn.com>
2018-02-09 18:22:36 +0100

    updated classifier2 and fixed several issues

---
+++ Classifier2/testscript2.5D/PythonApplication5/ConvNet.py
@@ -0,0 +1,141 @@
+import io
+import numpy as np
+
+f = open('../../../Data2/properties.txt');
+trainSize = int(f.readline())
+evalSize = int(f.readline())
+evalSmallSize = int(f.readline())
+evalLargeSize = int(f.readline())
+xtrain = np.zeros((trainSize,16,16,16), dtype=np.float32)
+ytrain = np.zeros(trainSize, dtype=np.float32)
+xeval = np.zeros((evalSize,16,16,16), dtype=np.float32)
+yeval = np.zeros(evalSize, dtype=np.float32)
+xsmalleval = np.zeros((evalSmallSize,16,16,16), dtype=np.float32)
+ysmalleval = np.zeros(evalSmallSize, dtype=np.float32)
+xlargeeval = np.zeros((evalLargeSize,16,16,16), dtype=np.float32)
+ylargeeval = np.zeros(evalLargeSize, dtype=np.float32)
+trainName = np.chararray(trainSize)
+evalName = np.chararray(evalSize)
+evalLargeName = np.chararray(evalSmallSize)
+evalSmallName = np.chararray(evalLargeSize)
+#trainName = ["" for x in range(trainSize)]
+#evalName = ["" for x in range(evalSize)]
+
+count = 0
+
+
+f = open('../../../Data2/TrainingData.txt', 'r')
+
+for line in f:
+    if count%100 == 0:
+        print(count)
+    A=np.zeros( (16,16,16) )
+    ## print(A)
+    itt=0
+    for i in range(0,16):
+        for j in range(0,16):
+            for k in range(0,16):
+                A[i,j,k] = line[itt]
+                itt = itt + 1
+    itt2 = -2
+    i = 1
+    end = 0
+    while True:
+        if (line[itt2] == ' '):
+            break        
+        end = end + int(line[itt2])*i
+        i = i*10
+        itt2 = itt2 -1
+    trainName[count]=line[itt:itt2]
+    xtrain[count,:,:,:] = A
+    ytrain[count]=end
+    count = count +1
+    if count >= trainSize:
+        break
+    
+f = open('../../../Data2/GoalData.txt', 'r')    
+count = 0    
+for line in f:
+    if count%100 == 0:
+        print(count)
+    A=np.zeros( (16,16,16) )
+    ## print(A)
+    itt=0
+    for i in range(0,16):
+        for j in range(0,16):
+            for k in range(0,16):
+                A[i,j,k] = line[itt]
+                itt = itt + 1
+    itt2 = -2
+    i = 1
+    end = 0
+    while True:
+        if (line[itt2] == ' '):
+            break        
+        end = end + int(line[itt2])*i
+        i = i*10
+        itt2 = itt2 -1
+    evalName[count]=line[itt:itt2]
+    xeval[count,:,:,:] = A
+    yeval[count]=end
+    count = count +1
+    if count >= evalSize:
+        break
+
+f = open('../../../Data2/SmallGoalData.txt', 'r')    
+count = 0    
+for line in f:
+    if count%100 == 0:
+        print(count)
+    A=np.zeros( (16,16,16) )
+    ## print(A)
+    itt=0
+    for i in range(0,16):
+        for j in range(0,16):
+            for k in range(0,16):
+                A[i,j,k] = line[itt]
+                itt = itt + 1
+    itt2 = -2
+    i = 1
+    end = 0
+    while True:
+        if (line[itt2] == ' '):
+            break        
+        end = end + int(line[itt2])*i
+        i = i*10
+        itt2 = itt2 -1
+    evalSmallName[count]=line[itt:itt2]
+    xsmalleval[count,:,:,:] = A
+    ysmalleval[count]=end
+    count = count +1
+    if count >= evalSize:
+        break
+
+f = open('../../../Data2/LargeGoalData.txt', 'r')    
+count = 0    
+for line in f:
+    if count%100 == 0:
+        print(count)
+    A=np.zeros( (16,16,16) )
+    ## print(A)
+    itt=0
+    for i in range(0,16):
+        for j in range(0,16):
+            for k in range(0,16):
+                A[i,j,k] = line[itt]
+                itt = itt + 1
+    itt2 = -2
+    i = 1
+    end = 0
+    while True:
+        if (line[itt2] == ' '):
+            break        
+        end = end + int(line[itt2])*i
+        i = i*10
+        itt2 = itt2 -1
+    evalLargeName[count]=line[itt:itt2]
+    xlargeeval[count,:,:,:] = A
+    ylargeeval[count]=end
+    count = count +1
+    if count >= evalLargeSize:
+        break
---
+++ Classifier2/testscript2.5D/PythonApplication5/Repeater.py
@@ -0,0 +1,20 @@
+import sys
+import os
+import shutil
+import testscript
+# this function runs the network x times with y steps and saves it every z runs
+if __name__ == "__main__":
+    x = int(sys.argv[1])
+    y = int(sys.argv[2])
+    z = int(sys.argv[3])
+    #call = "python testscript.py " + str(y)
+    storage_location = os.environ['HOME']+"/storage/Classifier2/2.5D/"
+    testscript.numOfSteps=y
+    for i in range(0,x):
+        # os.system(call)
+        testscript.main([]  )
+        if (i%z == z-1 or i == x-1):
+            shutil.copytree("/tmp/Classifier2/2.5D", storage_location+str(y*(i+1)), ignore=None)
+        else:
+            os.makedirs(storage_location+str(y*(i+1)))
+        shutil.copy("./results.txt",storage_location+str(y*(i+1)))
